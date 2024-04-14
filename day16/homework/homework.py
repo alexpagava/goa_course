07840b219826fbf2590c78f0c478c789a03be587
alexpagava <159301882+alexpagava@users.noreply.github.com>
2024-04-15 00:13:06 +0400

    add more folders

---
+++ day16/homework/homework.py
@@ -0,0 +1,54 @@
+# family_mebers=['teo','mia','alex','elusa','jamlet']
+
+# print(family_mebers[0])
+# print(family_mebers[1])
+# print(family_mebers[2])
+# print(family_mebers[3])
+# print(family_mebers[4])
+
+
+# numbers=[1,2,3,4,5,6,7,8,9,10]
+
+# print(numbers[0])
+# print(numbers[9])
+
+# numbers=[10,11,12,13,14,15,16,17,18,19,20]
+
+# numbers[0]=1
+# numbers[2]=1
+# numbers[4]=1
+# numbers[6]=1
+# numbers[8]=1
+# numbers[10]=1
+
+# print(numbers)
+
+# information=[]
+
+
+# name=input("Enter your name")
+# surname=("Enter your surname")
+# age=("Enter you age")
+# living_place=("Enter your living place")
+# mail=("Enter your mail")
+
+
+# information.append(name)
+# information.append(surname)
+# information.append(age)
+# information.append(living_place)
+# information.append(mail)
+
+# print(information)
+
+
+
+# surname="paghava"
+
+# print(surname[0])
+# print(surname[1])
+# print(surname[2])
+# print(surname[3])
+# print(surname[4])
+# print(surname[5])
+# print(surname[6])
